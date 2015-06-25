0810178a83ffba86e9c3f95e8c26568913259216
Jacky <fusijie@vip.qq.com>
2015-06-25 14:57:10 +0800

    update release note. +time

---
+++ build_to_ghpage.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+mkdir release_temp
+gitbook build --output=release_temp
+cp -r release_temp/ release/
+rm -rf release_temp
+cd release
+rm -r release_temp
+git add .
+time=`time`
+git commit -m "update release note. "+time
