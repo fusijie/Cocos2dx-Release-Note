2b73e2696ae34d3001218e9c77a6e120e6abc033
Jacky <fusijie@vip.qq.com>
2015-06-25 15:05:02 +0800

    add build_to_ghpages shell.

---
+++ build_to_ghpage.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+mkdir release_temp
+gitbook build --output=release_temp
+cp -r release_temp/ release/
+rm -rf release_temp
+cd release
+rm -r release_temp
+git add .
+time=`date`
+git commit -m "update release note at ${time}"
+git push origin gh-pages
