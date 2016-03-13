33149bb0fd2e4be70e0d7681716f8c4512f1c5f0
fusijie <fusijie@vip.qq.com>
2016-03-13 15:43:15 +0800

    update script.

--- build_to_ghpage.sh
+++ build_to_ghpage.sh
@@ -4,10 +4,9 @@ gitbook build
 cp -r _book/ release/
 rm -rf _book
 rm release/build_to_ghpage.sh
-g checkout gh-pages
+git checkout gh-pages
 cp -rf release/ .
 git add .
 time=`date`
 git commit -m "update release note at ${time}"
 git push origin gh-pages
-git checkout master
