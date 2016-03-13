424e3025e8f8ed5ad8b2239cb279c6b19a5a0343
fusijie <fusijie@vip.qq.com>
2016-03-13 15:10:26 +0800

    update build script.

--- build_to_ghpage.sh
+++ build_to_ghpage.sh
@@ -1,10 +1,8 @@
 #! /bin/sh
-mkdir release_temp
-gitbook build --output=release_temp
-cp -r release_temp/ release/
-rm -rf release_temp
+gitbook build 
+cp -r _book/ release/
+rm -rf _book
 cd release
-rm -r release_temp
 rm build_to_ghpage.sh
 git add .
 time=`date`
